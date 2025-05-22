e92149c5e3aec8281aec5b28404b2597ea0473a8
D[oa]vid Weisz <dave@dweisz.com>
2025-05-22 22:47:48 +0000

    Format example directory

--- example/webpack.config.js
+++ example/webpack.config.js
@@ -1,50 +1,53 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
-
-
-function createWebpackConfig({theme}, argv) {
-  return {
-    mode: argv.mode || 'development',
-    entry: './src/index.js',
-    output: {
-      filename: 'main.js',
-      path: path.resolve(__dirname, 'dist'),
-      clean: true,
-    },
-    module: {
-      rules: [
-        {
-          test: /\.(js|jsx)$/,
-          exclude: /node_modules/,
-          use: {
-            loader: 'babel-loader',
-            options: {
-              presets: ['@babel/preset-env', '@babel/preset-react'],
-            },
-          },
-        },
-        {
-          test: /\.css$/,
-          use: ['style-loader', 'css-loader'],
-        },
-      ],
-    },
-    plugins: [
-      new HtmlWebpackPlugin({
-        template: './public/index.html',
-      }),
-    ],
-    devServer: {
-      static: {
-        directory: path.join(__dirname, 'dist'),
-      },
-      compress: true,
-      port: 9000,
-    },
-  };
+function createWebpackConfig({ theme }, argv) {
+	return {
+		mode: argv.mode || "development",
+		entry: "./src/index.js",
+		output: {
+			filename: "main.js",
+			path: path.resolve(__dirname, "dist"),
+			clean: true,
+		},
+		module: {
+			rules: [
+				{
+					test: /\.(js|jsx)$/,
+					exclude: /node_modules/,
+					use: {
+						loader: "babel-loader",
+						options: {
+							presets: [
+								"@babel/preset-env",
+								"@babel/preset-react",
+							],
+						},
+					},
+				},
+				{
+					test: /\.css$/,
+					use: ["style-loader", "css-loader"],
+				},
+			],
+		},
+		plugins: [
+			new HtmlWebpackPlugin({
+				template: "./public/index.html",
+			}),
+		],
+		devServer: {
+			static: {
+				directory: path.join(__dirname, "dist"),
+			},
+			compress: true,
+			port: 9000,
+		},
+	};
 }
 
 module.exports = (env, argv) => {
-  return createVariantConfig(variantSettings => createWebpackConfig(variantSettings, argv));
-}
+	return createVariantConfig(variantSettings =>
+		createWebpackConfig(variantSettings, argv)
+	);
+};
